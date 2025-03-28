45e951203d341549169fa5036e7a66b7fc2b8289
onepunchkim <hyukjin4711@gmail.com>
2025-03-28 15:13:54 +0900

    addMemberForm

--- src/main/java/hello/item_service/TestDataInit.java
+++ src/main/java/hello/item_service/TestDataInit.java
@@ -2,6 +2,8 @@ package hello.item_service;
 
 import hello.item_service.domain.item.Item;
 import hello.item_service.domain.item.ItemRepository;
+import hello.item_service.domain.member.Member;
+import hello.item_service.domain.member.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /*
      * 테스트용 데이터 추가
@@ -19,5 +22,11 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("testA", 10000, 10));
         itemRepository.save(new Item("testB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+        memberRepository.save(member);
     }
 }
