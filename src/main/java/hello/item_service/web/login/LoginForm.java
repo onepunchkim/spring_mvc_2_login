c1bd57f847ec201f1e2040b8a82a3086d9434df4
onepunchkim <hyukjin4711@gmail.com>
2025-03-28 15:39:11 +0900

    loginForm

---
+++ src/main/java/hello/item_service/web/login/LoginForm.java
@@ -0,0 +1,13 @@
+package hello.item_service.web.login;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class LoginForm {
+
+    @NotEmpty
+    private String loginId;
+    @NotEmpty
+    private String password;
+}
