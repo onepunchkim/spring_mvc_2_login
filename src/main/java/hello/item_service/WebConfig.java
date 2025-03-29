4fdd831ada07d779fa493154c965f78f84d62a25
onepunchkim <hyukjin4711@gmail.com>
2025-03-29 17:32:03 +0900

    servlet filter

---
+++ src/main/java/hello/item_service/WebConfig.java
@@ -0,0 +1,21 @@
+package hello.item_service;
+
+import hello.item_service.web.filter.LogFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+}
